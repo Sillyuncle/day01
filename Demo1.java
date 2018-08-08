de0bff5d55ffa2af9fb220f4a34a79cb560c0e8b
Sillyuncle <33867665+Sillyuncle@users.noreply.github.com>
2018-08-08 23:42:47 +0800

    课堂案例

---
+++ Demo1.java
@@ -0,0 +1,9 @@
+public class Demo1{
+	public static void main(String[] args){
+	System.out.print("商品\t单价\t数量\t总价\n");
+	System.out.print("铅笔\t1.2\t10\t12\n");
+	System.out.print("铅笔1\t11.2\t10\t112\n");
+	//	System.out.println("你好\n我的祖国");
+	//	System.out.print("I Love You!");
+	}
+}
